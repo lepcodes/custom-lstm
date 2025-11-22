9517d584b7d5a5cad51e7cc3f87ccf07d5181409
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2025-11-21 19:22:29 -0600

    Remove unused losses

--- custom_lstm/losses/acf_losses.py
+++ custom_lstm/losses/acf_losses.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 from torch import nn
 
@@ -48,39 +47,3 @@ class EWACFLoss(nn.Module):
         self.variance.fill_(self.epsilon)
         self.covariance.zero_()
         self.input_lag = torch.tensor([], device=self.mean.device)
-
-
-def ew_acf(time_series, lag, lambda_=0.5, last_only=False):
-    """
-    Calculates the exponential weighted autocorrelation function of a time series.
-    Args:
-        time_series (array-like): The time series to calculate the autocorrelation function of.
-        lag (int): The lag to calculate the autocorrelation at.
-        lambda_ (float): The exponential decay parameter.
-    Returns:
-        float: The autocorrelation value with specified lag.
-    """
-    if len(time_series) <= lag:
-        return np.nan
-
-    mean = 0.5
-    variance = 0
-    variance_lag = 0
-    autocovariance = 0
-
-    if not last_only:
-        autocorrerlation_list = []
-
-    for i in range(lag, len(time_series)):
-        mean = lambda_ * mean + (1 - lambda_) * time_series[i]
-        autocovariance = lambda_ * autocovariance + (1 - lambda_) * (time_series[i] - mean) * (time_series[i - lag] - mean)
-        variance = lambda_ * variance + (1 - lambda_) * (time_series[i] - mean) ** 2
-        variance_lag = lambda_ * variance_lag + (1 - lambda_) * (time_series[i - lag] - mean) ** 2
-        autocorrelation = autocovariance / np.sqrt(variance * variance_lag)
-        if not last_only:
-            autocorrerlation_list.append(autocorrelation)
-
-    if not last_only:
-        return np.array(autocorrerlation_list)
-
-    return autocorrelation
